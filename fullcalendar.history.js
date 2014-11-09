ce1367133f5583eac646c47dde6f41180d8b125b
Jan Ptacek <jan.ptacek@gmail.com>
2014-11-10 00:09:41 +0100

    saving into cookie, reset on today button

--- fullcalendar.history.js
+++ fullcalendar.history.js
@@ -14,16 +14,28 @@ $(function() {
 	window.FullCalendarHistory = {};
 	FullCalendarHistory.viewRender = function (view, element) {
 		if (history && FullCalendarHistory.on_navigation) {
-			var view_name;
-			if (view.name == 'month') {
-				view_name = 'month';
-			} else if (view.name == 'agendaWeek') {
-				view_name = 'week';
-			} else if (view.name == 'agendaDay') {
-				view_name = 'day';
+			var url;
+			if (event && event.target && $(event.target).hasClass('fc-today-button')) {
+				// today button = clear remembered date
+				url = location.pathname.replace(/\/(month|week|day)\/[-0-9]{10}/, '');
+				history.pushState(null, null, url);
+				$.removeCookie('fullcalendar-history-view-name', { path: '/' });
+				$.removeCookie('fullcalendar-history-start', { path: '/' });
+			} else {
+				var view_name;
+				if (view.name == 'month') {
+					view_name = 'month';
+				} else if (view.name == 'agendaWeek') {
+					view_name = 'week';
+				} else if (view.name == 'agendaDay') {
+					view_name = 'day';
+				}
+				var start = view.intervalStart.format('YYYY-MM-DD');
+				url = location.pathname.replace(/\/(month|week|day)\/[-0-9]{10}/, '') + '/' + view_name + '/' + start;
+				history.pushState(null, null, url);
+				$.cookie('fullcalendar-history-view-name', view_name, { expires: 7, path: '/' });
+				$.cookie('fullcalendar-history-start', start, { expires: 7, path: '/' });
 			}
-			var url = location.pathname.replace(/\/(month|week|day)\/[-0-9]{10}/, '') + '/' + view_name + '/' + view.intervalStart.format('YYYY-MM-DD');
-			history.pushState(null, null, url);
 		}
 		FullCalendarHistory.on_navigation = 1;
 	};
